443f1c0e121d889f467fd3559253c4613b4c6886
Anas Khan <107175123+technonies@users.noreply.github.com>
2022-07-16 15:11:40 -0700

    v3

--- main.tf
+++ main.tf
@@ -1,16 +1,49 @@
-terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 3.0"
-   }
- }
+resource "aws_security_group" "security_jenkins_ports" {
+  name        = "security_jenkins_ports"
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+#t
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "security_jenkins_port"
+  }
 }
-resource "aws_instance" "demo" {
- ami = var.ami
- instance_type = var.type
- 
- tags = {
-   name = "Demo System"
- }
+
+resource "aws_instance" "myFirstInstance" {
+  ami           = "ami-052efd3df9dad4825"
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [ "security_jenkins_port"]
+  tags= {
+    Name = "jenkins_instance"
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "jenkins_elstic_ip"
+  }
 }
